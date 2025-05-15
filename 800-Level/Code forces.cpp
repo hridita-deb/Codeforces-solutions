93f88a80f6f54b42c11d18d3134574692df2f30f
Hridta Chandra Deb <debhridita9@gmail.com>
2025-05-15 21:35:45 +0600

    Create Code forces.cpp

---
+++ 800-Level/Code forces.cpp
@@ -0,0 +1,30 @@
+//Watermelon
+
+#include<iostream>
+using namespace std;
+int main()
+{
+    int weight;
+    cin>>weight;
+    if(weight%2!=0)
+        cout<<"No";
+    else if(weight==2)
+        cout<<"No";
+    else
+        cout<<"Yes";
+
+    return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
