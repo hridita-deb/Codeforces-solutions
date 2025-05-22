2248d0bd274602c52c19f1b8b386802b8590792c
Hridta Chandra Deb <debhridita9@gmail.com>
2025-05-22 19:16:44 +0600

    Create Short Substrings.cpp

---
+++ 800-Level/Short Substrings.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+
+
+        vector<char>v;
+        v.push_back(s[0]);
+        for(int i=1; i<s.size(); i=i+2)
+        {
+
+                v.push_back(s[i]);
+
+        }
+
+
+        for(int i=0; i<v.size(); i++)
+        {
+            cout<<v[i];
+        }
+        cout<<endl;
+    }
+
+
+
+
+
+
+
+
+
+
+}
