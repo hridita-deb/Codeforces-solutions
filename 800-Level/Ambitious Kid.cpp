1dc1ab55054c817e6657f9d924045ca75380d6fd
Hridta Chandra Deb <debhridita9@gmail.com>
2025-05-27 21:25:42 +0600

    Create Ambitious Kid.cpp

---
+++ 800-Level/Ambitious Kid.cpp
@@ -0,0 +1,40 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+      for(int i=0; i<n; i++)
+    {
+        v[i]=abs(v[i]);
+    }
+    int min=v[0];
+    for(int i=1; i<n; i++)
+    {
+        if(min>v[i])
+        {
+            min=v[i];
+        }
+    }
+    cout<<min<<endl;
+    int move=abs(min);
+    cout<<move<<endl;
+}
+
+
+
+
+
+
+
+
+
+
+
+
