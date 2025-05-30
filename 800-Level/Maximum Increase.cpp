5d5a902a5bed1d8f6f0017b0251bf5c8cddc2750
Hridta Chandra Deb <debhridita9@gmail.com>
2025-05-30 21:46:12 +0600

    Add 2 code files

---
+++ 800-Level/Maximum Increase.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+
+    int n;
+    cin>>n;
+    vector<int>v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+
+
+    int curSize=1,maxSize=1;
+    for(int i=0; i<n-1; i++)
+    {
+
+        if(v[i]<v[i+1])
+        {
+            curSize++;
+        }
+        else
+        {
+            curSize=1;
+        }
+        maxSize=max(maxSize,curSize);
+    }
+
+    cout<<maxSize<<endl;
+
+}
