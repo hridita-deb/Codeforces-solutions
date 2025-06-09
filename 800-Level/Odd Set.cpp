f6b735119c3d4fe46feb98d20458a310d950b9e6
Hridta Chandra Deb <debhridita9@gmail.com>
2025-06-09 22:19:34 +0600

    Update Odd Set.cpp

--- 800-Level/Odd Set.cpp
+++ 800-Level/Odd Set.cpp
@@ -1,1 +1,46 @@
 #include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n;
+        cin>>n;
+
+
+        int p=2*n;
+        vector<int>v(p);
+        for ( int i=0; i<p; i++)
+        {
+            cin>>v[i];
+        }
+
+
+        int odd=0,even=0;
+        for ( int i=0; i<p; i++)
+        {
+            if(v[i]%2==0)
+            {
+                even++;
+            }
+            else
+            {
+                odd++;
+            }
+        }
+
+
+        if(even==n && odd==n)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
