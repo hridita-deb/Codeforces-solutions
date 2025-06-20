bc6a211bd7495188b0cf2a7f7d2b48825cd3294d
Hridta Chandra Deb <debhridita9@gmail.com>
2025-06-20 12:16:00 +0600

    Update Clock and Strings.cpp

--- 900- Level/Clock and Strings.cpp
+++ 900- Level/Clock and Strings.cpp
@@ -1,1 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int a,b,c,d;
+        cin>>a>>b>>c>>d;
+
+        int minimum=min(a,b);
+        int maximum=max(a,b);
+
+        vector<int>v;
+
+        for(int i=minimum; i<=maximum; i++)
+        {
+            v.push_back(i);
+        }
+
+
+        int found_c=0,found_d=0;
+        for(int i=0; i<v.size(); i++)
+        {
+            if(c==v[i])
+            {
+                found_c=1;
+            }
+            if(d==v[i])
+            {
+                found_d=1;
+            }
+        }
+
+        if((found_c==1 && found_d==0) || (found_c==0 && found_d==1))
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+    }
+
+}
+
+
+
+
 
