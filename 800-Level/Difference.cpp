b726fdc821dedacfc80eb063d57e75d7432c89e1
Hridta Chandra Deb <debhridita9@gmail.com>
2025-07-04 22:56:14 +0600

    Create Difference.cpp

---
+++ 800-Level/Difference.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    long long a,b,c,d;
+    cin>>a>>b>>c>>d;
+
+    long long ab,cd;
+    ab=a*b;
+    cd=c*d;
+
+    long long x=(ab)-(cd);
+    cout<<"Difference = "<<x<<endl;
+
+}
