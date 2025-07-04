7b7139bd3c4c5f8b2d90598b31b5b71ed89512c8
Hridta Chandra Deb <debhridita9@gmail.com>
2025-07-04 22:29:24 +0600

    Create Basic Data Types.cpp

---
+++ 800-Level/Basic Data Types.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int num;
+    long long num2;
+    char ch;
+    float num3;
+    double num4;
+    cin>>num>>num2>>ch>>num3>>num4;
+    cout<<num<<endl;
+    cout<<num2<<endl;
+    cout<<ch<<endl;
+    cout<<num3<<endl;
+    cout<<num4<<endl;
+
+}
