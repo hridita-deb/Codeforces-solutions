cfcb5c6c6613d3f06fff194fc4b10e43ee079707
Hridta Chandra Deb <debhridita9@gmail.com>
2025-07-07 22:34:17 +0600

    Create Digits Summation.cpp

---
+++ 800-Level/Digits Summation.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string a,b;
+    cin>>a>>b;
+
+    int num1=a[a.size()-1]-'0';
+    int num2=b[b.size()-1]-'0';
+
+    cout<<num1+num2<<endl;
+
+}
