dd1cd1f1962ade528bfdccacc1827cdddcac5306
Hridta Chandra Deb <debhridita9@gmail.com>
2025-07-17 21:21:26 +0600

    Create Easy Problem.cpp

---
+++ 800-Level/Easy Problem.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+    int t,n,i=0;
+    cin>>t;
+    cout<<t<<endl;
+    if(t>=1 && t<=99)
+    {
+        int array[t];
+        for(i=0; i<t; i++)
+        {
+
+            cin>>array[i];
+        }
+
+        for(i=0; i<t; i++)
+        {
+           // cout<<array[i]<<endl;
+            if(array[i]>=2 && array[i]<=100)
+            {
+                int count=0;
+                for(int b=1; b<array[i]; b++)
+                {
+
+                    count++;
+                }
+                cout<<count<<endl;
+            }
+
+        }
+
+
+    }
+
+    else
+    {
+        cout<<"Error"<<endl;
+    }
+
+}
