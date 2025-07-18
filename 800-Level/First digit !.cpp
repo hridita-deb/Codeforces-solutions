de71aa4b1b75af43c8938a88fb911908d62872a7
Hridta Chandra Deb <debhridita9@gmail.com>
2025-07-18 17:25:05 +0600

    Create First digit !.cpp

---
+++ 800-Level/First digit !.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string s;
+    cin>>s;
+
+    int num=s[0]-'0';
+
+    if(num%2!=0)
+    {
+        cout<<"ODD"<<endl;
+    }
+    else
+    {
+        cout<<"EVEN"<<endl;
+    }
+
+}
