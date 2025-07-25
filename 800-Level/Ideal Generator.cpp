0c44848949852e1670e954937039bb3b10e9f0ea
Hridta Chandra Deb <debhridita9@gmail.com>
2025-07-25 21:44:01 +0600

    Create Ideal Generator.cpp

---
+++ 800-Level/Ideal Generator.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int k;
+        cin>>k;
+        if(k%2!=0)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
