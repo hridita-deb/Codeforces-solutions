64226004afe85f70a6f927df072e9bcb8d265adb
Hridta Chandra Deb <debhridita9@gmail.com>
2025-07-30 22:05:32 +0600

    Create ABC.cpp

---
+++ 800-Level/ABC.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main ()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+        if(n>2 || s=="00" || s=="11")
+        {
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+    }
+}
