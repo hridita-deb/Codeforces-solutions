2d708d9869732d57ad7689fe63a63aad38011459
Hridta Chandra Deb <debhridita9@gmail.com>
2025-08-04 21:23:18 +0600

    Create Skibidus and Ohio .cpp

---
+++ 800-Level/Skibidus and Ohio .cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        string s;
+        cin>>s;
+
+        int count=0;
+        for(int i=1; i<s.size(); i++)
+        {
+            if(s[i]==s[i-1])
+            {
+                count++;
+            }
+        }
+
+        if(count==0)
+        {
+            cout<<s.size()<<endl;
+        }
+        else
+        {
+            cout<<"1"<<endl;
+        }
+
+    }
+}
