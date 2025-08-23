d3ca7cfc3eae2fb51bc942734d221fb0a86fa24e
Hridta Chandra Deb <debhridita9@gmail.com>
2025-08-23 19:08:50 +0600

    Create EhAb AnD gCd.cpp

---
+++ 800-Level/EhAb AnD gCd.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        int x;
+        cin>>x;
+
+        int a=1;
+        int b=x-1;
+
+        cout<<a<<" "<<b<<endl;
+    }
+}
