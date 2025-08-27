e6de0ce4e89b3298beac1ba60b131778aaaeea95
Hridta Chandra Deb <debhridita9@gmail.com>
2025-08-27 15:12:14 +0600

    Create File Name.cpp

---
+++ 800-Level/File Name.cpp
@@ -0,0 +1,46 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n;
+    cin>>n;
+    string s;
+    cin>>s;
+
+    int count =0;
+    int a=0;
+
+    for(int i=0; i<n; i++)
+    {
+
+        if(s[i]=='x')
+        {
+            count++;
+            if(count>2)
+            {
+                a++;
+            }
+        }
+
+        else
+        {
+            count=0;
+        }
+
+    }
+
+    cout<<a<<endl;
+
+}
+
+
+
+
+
+
+
+
+
+
+
