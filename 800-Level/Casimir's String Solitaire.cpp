a7d5c6d879017a7d13c9139db27b21a44afa69b1
Hridta Chandra Deb <debhridita9@gmail.com>
2025-08-30 00:01:25 +0600

    Create Casimir's String Solitaire.cpp

---
+++ 800-Level/Casimir's String Solitaire.cpp
@@ -0,0 +1,43 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        string s;
+        cin>>s;
+
+        int a=0,b=0,c=0;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]=='A')
+            {
+                a++;
+            }
+            else  if(s[i]=='B')
+            {
+                b++;
+            }
+            else
+            {
+                c++;
+            }
+        }
+
+
+        if(b==a+c)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+
+    }
+}
