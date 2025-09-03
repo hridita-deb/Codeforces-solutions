727eca6cadbba4939bc5a0aca5c064de0c4ab41c
Hridta Chandra Deb <debhridita9@gmail.com>
2025-09-04 00:03:37 +0600

    Create osu!mania.cpp

---
+++ 800-Level/osu!mania.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+
+        vector<int>v;
+
+        while(n--)
+        {
+            string s;
+            cin>>s;
+            for(int i=0; i<s.size(); i++)
+            {
+                if(s[i]=='#')
+                {
+                    v.push_back(i+1);
+                }
+            }
+        }
+
+        reverse(v.begin(),v.end());
+
+        for(auto it: v)
+        {
+            cout<<it<<" ";
+        }
+        cout<<endl;
+    }
+
+}
