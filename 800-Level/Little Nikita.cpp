7730fc667a18a03e1564f54613640e2a86856395
Hridta Chandra Deb <debhridita9@gmail.com>
2025-09-03 23:52:54 +0600

    Create Little Nikita.cpp

---
+++ 800-Level/Little Nikita.cpp
@@ -0,0 +1,30 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m;
+        cin>>n>>m;
+
+
+        if(n>=m)
+        {
+            int a=n-m;
+            if(a%2==0)
+            {
+                cout<<"YES"<<endl;
+            }
+            else
+            {
+                cout<<"NO"<<endl;
+            }
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+    }
+}
