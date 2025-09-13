27f0fc048f89d32d10e0f7da34b886aa303d853e
Hridta Chandra Deb <debhridita9@gmail.com>
2025-09-13 22:46:19 +0600

    Lasers

---
+++ 800-Level/2148B.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n,m,x,y;
+        cin>>n>>m>>x>>y;
+        vector<int>p(n);
+        for(int i=0; i<n; i++)
+        {
+            cin>>p[i];
+        }
+        vector<int>q(m);
+        for(int i=0; i<m; i++)
+        {
+            cin>>q[i];
+        }
+
+cout<<n+m<<endl;
+
+    }
+}
+
