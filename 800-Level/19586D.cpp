5e7eea01f8099b74b7e759fd0e4c8e16ac932535
Hridta Chandra Deb <debhridita9@gmail.com>
2025-09-14 11:21:15 +0600

    Create 19586D.cpp

---
+++ 800-Level/19586D.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string a,b;
+    cin>>a>>b;
+    string c=a+b;
+
+    cout<<a.size()<<" "<<b.size()<<endl;
+    cout<<c<<endl;
+    swap(a[0],b[0]);
+    cout<<a<<" "<<b<<endl;
+}
