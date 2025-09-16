32a4f22007e6142a9d4e99f0b7095aba12ac16b7
Hridta Chandra Deb <debhridita9@gmail.com>
2025-09-17 00:05:45 +0600

    Square or rectangle

---
+++ 800-Level/329103A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+
+    {
+        string a, b;
+        cin >> a >> b;
+
+        if (a == b)
+        {
+            cout << "Square" << endl;
+        }
+        else
+        {
+            cout << "Rectangle" << endl;
+        }
+    }
+}
