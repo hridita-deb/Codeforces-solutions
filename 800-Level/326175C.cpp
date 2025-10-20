0c7d7878b59ffdf42104cc0e0c9e8c330794b02e
Hridta Chandra Deb <debhridita9@gmail.com>
2025-10-20 22:22:56 +0600

    Create 326175C.cpp

---
+++ 800-Level/326175C.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    char s;
+    cin>>s;
+
+    if(s=='z')
+    {
+        cout<<"a";
+
+    }
+    else
+    {
+        char ans=s+1;
+        cout<<ans;
+    }
+}
