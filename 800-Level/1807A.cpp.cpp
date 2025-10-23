c19fc860ccd48c8085f25602c004be52afb510d0
Hridta Chandra Deb <debhridita9@gmail.com>
2025-10-23 23:41:49 +0600

    Plus or Minus

---
+++ 800-Level/1807A.cpp.cpp
@@ -0,0 +1,56 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+ int t,a,b,c;
+ cin>>t;
+ for(int i=0;i<t;i++)   {
+    cin>>a>>b>>c;
+    int e=a+b;
+    int f=a-b;
+
+    if(e==c){
+        cout<<"+"<<endl;
+    }
+    if(f==c){
+        cout<<"-"<<endl;
+    }
+ }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
