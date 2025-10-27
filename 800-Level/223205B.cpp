47ec0db91b986090754ff11488cc7a4134a90355
Hridta Chandra Deb <debhridita9@gmail.com>
2025-10-27 23:54:32 +0600

    Create 223205B.cpp

---
+++ 800-Level/223205B.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void print (int n)
+{
+    for(int i=1; i<=n; i++)
+    {
+        cout<<i;
+        if(i<n)
+        {
+            cout<<" ";
+        }
+    }
+
+}
+int main()
+{
+    int n;
+    cin>>n;
+    print(n);
+}
