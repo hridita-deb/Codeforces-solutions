0612e17854c75d61d1568bd847402f94fd1569c3
Hridta Chandra Deb <debhridita9@gmail.com>
2025-10-28 16:34:30 +0600

    Create 223205E.cpp

---
+++ 800-Level/223205E.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+void swp (int a,int b)
+{
+    swap(a,b);
+    cout<<a<<" "<<b<<endl;
+}
+
+int main()
+{
+    int a,b;
+    cin>>a>>b;
+
+    swp(a,b);
+}
