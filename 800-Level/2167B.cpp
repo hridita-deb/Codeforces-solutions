fe8c5284794b26912596df0dab335aeda9185968
Hridta Chandra Deb <debhridita9@gmail.com>
2025-10-30 23:37:33 +0600

    Your Name

---
+++ 800-Level/2167B.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+
+    int t;
+    cin >> t;
+    while (t--)
+    {
+
+        int n;
+        cin >> n;
+
+        string a, b;
+        cin >> a >> b;
+
+        sort(a.begin(), a.end());
+        sort(b.begin(), b.end());
+
+        if (a == b)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+
+    }
+}
