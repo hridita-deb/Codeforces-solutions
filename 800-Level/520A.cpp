8a58ea098ed891b628958bf83e953256f78fa63c
Hridta Chandra Deb <debhridita9@gmail.com>
2025-11-08 12:52:17 +0600

    Pangram

---
+++ 800-Level/520A.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+
+    int n;
+    cin>>n;
+
+    string s;
+    cin>>s;
+
+    set<char>st;
+    for(int i=0; i<n; i++)
+    {
+        st.insert(tolower(s[i]));
+    }
+
+    if(st.size()==26)
+    {
+        cout<<"Yes"<<endl;
+    }
+    else
+    {
+        cout<<"No"<<endl;
+    }
+}
