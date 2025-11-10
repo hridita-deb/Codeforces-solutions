38c5fb3cfe959c74a3c8e393fcc83a4bae0b9db3
Hridta Chandra Deb <debhridita9@gmail.com>
2025-11-10 23:42:13 +0600

    Mahmoud and Longest Uncommon Subsequence

---
+++ 800-Level/223206W.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    string a,b;
+    cin>>a>>b;
+    if(a==b)
+    {
+        cout<<-1<<endl;
+    }
+    else
+    {
+        if(a.size()>b.size())
+        {
+            cout<<a.size()<<endl;
+        }
+        else
+        {
+            cout<<b.size()<<endl;
+        }
+    }
+}
