f41db43b6bb8d92acf99602b9d8d9783e0bad28a
Hridta Chandra Deb <debhridita9@gmail.com>
2025-11-12 23:56:17 +0600

    Sum

---
+++ 800-Level/1742A.cpp.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+ int t;
+ cin>>t;
+ int a,b,c;
+ /*for(int i=0;i<t;i++)   {
+    cin>>a>>b>>c;
+
+
+}*/
+
+
+
+ for(int i=0;i<t;i++)   {
+        cin>>a>>b>>c;
+ if((a==b+c) || (b==a+c) || (c==a+b)){
+        cout<<"YES"<<endl;
+    }
+else{
+    cout<<"NO"<<endl;
+}
+
+ }
+}
