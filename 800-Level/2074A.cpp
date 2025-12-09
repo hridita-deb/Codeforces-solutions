b59e610760137a9d54b881866988f2bce9a56e51
Hridta Chandra Deb <debhridita9@gmail.com>
2025-12-09 22:45:41 +0600

    Draw a Square.cpp

---
+++ 800-Level/2074A.cpp
@@ -0,0 +1,22 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t,a,b,c,d;
+    cin>>t;
+    for(int i=0; i<t; i++)
+    {
+        cin>>a>>b>>c>>d;
+        if(a==b && b==c && c==d && a==d)
+        {
+            cout<<"Yes"<<endl;
+        }
+        else
+        {
+            cout<<"No"<<endl;
+        }
+
+
+    }
+}
