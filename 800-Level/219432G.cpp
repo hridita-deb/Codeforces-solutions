bbabe8c0c1e7e15134668e1252cb6d4221465d7d
Hridta Chandra Deb <debhridita9@gmail.com>
2025-12-20 23:38:28 +0600

    Create 219432G.cpp

---
+++ 800-Level/219432G.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long n;
+        cin >> n;
+        long long fact = 1;
+        for (int i = 1; i <=n; i++)
+        {
+            fact = fact * i;
+        }
+
+        cout << fact << endl;
+    }
+}
