ee7303a0bac171e4449fcc629e2af83a5817afab
Hridta Chandra Deb <debhridita9@gmail.com>
2025-12-30 21:23:24 +0600

    Only Pluses

---
+++ 800-Level/1992A.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int a,b,c;
+        cin>>a>>b>>c;
+
+        for(int i=1; i<=5; i++)
+        {
+
+            if(a<=b && a<=c)
+            {
+                a++;
+            }
+            else if(b<=a && b<=c)
+            {
+                b++;
+            }
+            else
+            {
+                c++;
+            }
+
+        }
+        cout<<a*b*c<<endl;
+    }
+}
