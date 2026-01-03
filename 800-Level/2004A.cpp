453a77aa3a8f80b00fa90092be2af21f71ff52ba
Hridta Chandra Deb <debhridita9@gmail.com>
2026-01-03 22:11:42 +0600

    Closest Point

---
+++ 800-Level/2004A.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+
+        }
+        if(n==2 && v[1]-v[0]>1)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+    }
+}
