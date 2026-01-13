30812b0a8c5c0c5df8faf35d6ea5d9580c6c1f39
Hridta Chandra Deb <debhridita9@gmail.com>
2026-01-13 23:19:20 +0600

    Three Piles of Candies

---
+++ 800-Level/1196A.cpp
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long a,b,c;
+        cin>>a>>b>>c;
+
+        long long sum=0;
+        sum=a+b+c;
+        cout<<sum/2<<endl;
+    }
+}
