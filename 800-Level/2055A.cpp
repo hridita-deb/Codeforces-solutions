479011047cc176f94b9219f92b2413201aa8f66f
Hridta Chandra Deb <debhridita9@gmail.com>
2026-01-17 19:52:51 +0600

    Two Frogs

---
+++ 800-Level/2055A.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n,a,b;
+        cin>>n>>a>>b;
+
+        if( a%2!=b%2)
+        {
+
+            cout<<"NO"<<endl;
+        }
+        else
+        {
+            cout<<"YES"<<endl;
+        }
+
+    }
+}
