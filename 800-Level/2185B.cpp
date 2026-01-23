05c4dff804bbfaa51f612c5a81959592d9621553
Hridta Chandra Deb <debhridita9@gmail.com>
2026-01-23 18:59:00 +0600

    Prefix Max

---
+++ 800-Level/2185B.cpp
@@ -0,0 +1,34 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n;
+        cin>>n;
+        vector<int>v(n);
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+        }
+        int max=v[0];
+        for(int i=1; i<n; i++)
+        {
+            if(max<v[i])
+            {
+                max=v[i];
+            }
+        }
+        long long ans=max*n;
+        cout<<ans<<endl;
+
+
+
+
+    }
+}
