a41961e311bd2b6e415f859a608efe306939b0fd
Hridta Chandra Deb <debhridita9@gmail.com>
2026-02-24 19:36:00 +0600

    Garden

---
+++ 900- Level/915A.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int n, k;
+    cin >> n >> k;
+    int ans = k;
+
+    for (int i = 0; i < n; i++)
+    {
+        int a;
+        cin >> a;
+
+        if (k % a == 0)
+        {
+            ans = min(ans, k / a);
+        }
+    }
+
+    cout << ans << endl;
+
+
+}
