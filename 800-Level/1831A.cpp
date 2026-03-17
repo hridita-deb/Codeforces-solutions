5a86acfe280d45a82882a982acda6a87fdc48985
Hridta Chandra Deb <debhridita9@gmail.com>
2026-03-17 23:20:47 +0600

    Twin Permutations

---
+++ 800-Level/1831A.cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        vector<int>v(n);
+
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+        }
+
+
+        int a=n+1;
+
+
+        for(auto it:v)
+        {
+            cout<<a-it<<" ";
+        }
+        cout<<endl;
+
+    }
+}
