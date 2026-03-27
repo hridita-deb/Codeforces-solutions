d872aa3b82729d1bc72ece51bd81c8f3e42eb0bd
Hridta Chandra Deb <debhridita9@gmail.com>
2026-03-28 01:54:39 +0600

    Swap and Delete

---
+++ 1000- Level/1913B.cpp
@@ -0,0 +1,64 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string s;
+        cin>>s;
+
+        int one=0,zero=0;
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]=='1')
+            {
+                one++;
+            }
+            else
+            {
+                zero++;
+            }
+        }
+
+        for(int i=0; i<s.size(); i++)
+        {
+            if(s[i]=='1')
+            {
+                if(zero>0)
+                {
+                    zero--;
+                }
+
+                else
+                {
+                    break;
+                }
+            }
+            else
+            {
+                if(one>0)
+                {
+                    one--;
+                }
+
+                else
+                {
+                    break;
+                }
+            }
+        }
+
+        if(one==0)
+        {
+            cout<<zero<<endl;
+        }
+        else
+        {
+            cout<<one<<endl;
+        }
+
+    }
+}
