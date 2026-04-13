724583420808365880fd48b8f708b61d8f2bf688
Hridta Chandra Deb <debhridita9@gmail.com>
2026-04-13 22:32:04 +0600

    Everybody Likes Good Arrays!

---
+++ 800-Level/1777A.cpp
@@ -0,0 +1,39 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        int n;
+        cin>>n;
+        vector<int>v(n);
+        for(int i=0; i<n; i++)
+        {
+            cin>>v[i];
+        }
+        int count=0;
+        for(int i=0; i<n-1; i++)
+        {
+            if(v[i]%2==1)
+            {
+                if(v[i+1]%2==1)
+                {
+                    count++;
+                }
+            }
+            else
+            {
+                if(v[i+1]%2==0)
+                {
+                    count++;
+                }
+            }
+        }
+
+
+        cout<<count<<endl;
+    }
+}
