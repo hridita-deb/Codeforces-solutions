1a8b1c7eb24fc3d33372e253280b00c2e71ec30c
Hridta Chandra Deb <debhridita9@gmail.com>
2026-04-14 21:56:37 +0600

    Specialty String

---
+++ 900- Level/2200C.cpp
@@ -0,0 +1,45 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+
+    int t;
+    cin>>t;
+    while(t--)
+    {
+
+        long long n;
+        cin>>n;
+        string s;
+        cin>>s;
+
+        stack<char>st;
+        for(int i=0; i<n; i++)
+        {
+
+            if(!st.empty() && st.top()==s[i])
+            {
+                st.pop();
+            }
+            else
+            {
+                st.push(s[i]);
+            }
+        }
+
+        if(st.empty())
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+        {
+            cout<<"NO"<<endl;
+        }
+
+
+
+
+
+
+    }
+}
