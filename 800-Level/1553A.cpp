f21802c155f30fea9bff198611af6675cc444f74
Hridta Chandra Deb <debhridita9@gmail.com>
2026-05-09 23:37:30 +0600

    Digits Sum

---
+++ 800-Level/1553A.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        long long ans=floor((n+1)/10);
+        cout<<ans<<endl;
+    }
+}
