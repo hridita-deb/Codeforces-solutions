7389f6f504378220304513a20e7dfc7fd4ab3a2e
Hridta Chandra Deb <debhridita9@gmail.com>
2026-05-11 23:59:01 +0600

    Vasya and Socks

---
+++ 900- Level/460A.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n,m;
+    cin>>n>>m;
+
+    int day=0;
+    while(n>0)
+    {
+        day++;
+        n--;
+        if(day%m==0)
+        {
+            n++;
+        }
+    }
+    cout<<day<<endl;
+}
