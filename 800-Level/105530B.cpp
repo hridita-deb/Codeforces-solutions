a90d065820732e6de846a54924475220c8494d10
Hridta Chandra Deb <debhridita9@gmail.com>
2026-06-11 22:54:18 +0600

    Modular MEX

---
+++ 800-Level/105530B.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        long long n;
+        cin>>n;
+        long long ans=(n+1)/2;
+        cout<<ans<<endl;
+    }
+}
