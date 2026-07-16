df458455770d8ae9f824771059abb14c8a74ec0e
Hridita Chandra Deb <debhridita9@gmail.com>
2026-07-16 13:17:47 +0600

    Divide and Conquer

---
+++ 800-Level/2241A.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int x, y;
+        cin >> x >> y;
+
+        if (x % y == 0)
+        {
+            cout << "Yes" << endl;
+        }
+        else
+        {
+            cout << "No" << endl;
+        }
+
+    }
+}
