f1beebb31dc5aa2fad6539935e92756b5a515ae5
Hridita Chandra Deb <debhridita9@gmail.com>
2026-07-31 23:58:46 +0600

    Fancy Fence

---
+++ 1100 - Level/313B.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string s;
+    cin>>s;
+
+
+    vector<int>prefix;
+    prefix.push_back(0);
+    int    count=0;
+    for(int i=1; i<s.size(); i++)
+    {
+        if(s[i]==s[i-1])
+        {
+            count++;
+        }
+        prefix.push_back(count);
+    }
+
+
+    int q;
+    cin>>q;
+    while(q--)
+    {
+        int l,r;
+        cin>>l>>r;
+        int ans=prefix[r-1]-prefix[l-1];
+        cout<<ans<<endl;
+    }
+}
