918108e93098fdb9e0a20773b581f7bf53fc0478
Hridita Chandra Deb <debhridita9@gmail.com>
2026-08-21 22:48:19 +0600

    Three Pairwise Maximums

--- 800-Level/1385A.cpp
+++ 800-Level/1385A.cpp
@@ -14,19 +14,15 @@ int main()
         v.push_back(c);
 
         sort(v.begin(),v.end());
-        if(v[1]!=v[2]){
+        if(v[1]!=v[2])
+        {
             cout<<"NO"<<endl;
         }
-        else{
+        else
+        {
             cout<<"YES"<<endl;
             cout<<v[0]<<" "<<v[0]<<" "<<v[2]<<endl;
         }
-
-
-
-
-
-
 
 
     }
